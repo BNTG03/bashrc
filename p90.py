dbbd6b280d620b4f0bf67de05955d42437303d93
BNTG03 <48709727+BNTG03@users.noreply.github.com>
2019-08-05 15:40:12 +0000

    Add files via upload

---
+++ p90.py
@@ -0,0 +1,25 @@
+print("""
+           .'''''''''.......,''''.                                                   
+          odddxOddxxxdxxxkdddOkkkk:                                                  
+         .OkkOkOx0kkOkOkkOO0kc::::k,                                                 
+         ;kxxxxO0Oxdoolc::;,'.;ooxxd;                                                
+         lOOO0k;ododddddddddooxdxxxxxc;;;;;;;;;;;;:::::coxllccccccccc::;;,,,,''',,'. 
+         dKKKX0dodllololllolollokxxdddclllllllllllodolodKNONX0xOOOOOO0OOOOOkkkxxxx0  
+  ,o::::;dkxxxk0XK0OOOOOOOOkkkkdokkkOKkxxxxxxxxxxxxxxdkk0XOOkkOOOOOOOOOOOOOOOOOOOkx  
+ .cc;::cccxkkxkOdkkOxxdddxxxxxxxkkkkkkkkkkkkkkkOOkkOkOOOOOOkkOOOOOOOOOOOOOOOOOOOOko  
+         ;OkkkOO0000000Kd;;cxddd0KKKKKXXx:;;;;,;:xXXKKKKKKKKKO0000000000000000O00kl  
+         ;kO0kxkOOkOO0l.    'ccclxOOOOX;          :kOOOOOkOOkckkkkOOOOOOOkkkkkkkkxc  
+         .O0.    ook0o.   .:ckxklkOkkkKxl;.       c:kkkkOkOOxckkkkOOOOOOOkkkkkkkkxl  
+          cc     :okk0Ol;codxOolld00OkkOOkOkl;'';oodkkkkkkkOko0OOOOOOOOOOOOOOOOOOOx  
+                  oOkxkxxkxxO'    .cO0OOOkkOOkxxxkkkkkkkkkxo::dxxxxxxxxxxxxkOOOkxkd  
+                   'oOOO0OOOOl       .:dO0000000000koc;..                            
+                      ..'''''.            .........                                  
+""")
+print('''
+                               mmmmmm    mmm   mm  mmmmmmmm     mmmm  
+                               ##""""##  ###   ##  """##"""   ##""""# 
+                               ##    ##  ##"#  ##     ##     ##       
+                               #######   ## ## ##     ##     ##  mmmm 
+                               ##    ##  ##  #m##     ##     ##  ""## 
+                               ##mmmm##  ##   ###     ##      ##mmm## 
+''')
---
+++ smg.py
@@ -0,0 +1,27 @@
+print("""
+                                   .                                            
+                                .cxdc,                 ,::.      ':c;.          
+                                :oxxoo;.....,:::::cc;;;okkl,,''.,xxxxl.         
+                       .','''ldxlllccc::;::;oXxxxxxx0loooooodkkxxxkkkkkx;;'      
+                   .:oxdolllldxkoldooool:loodkkOOOOkdxdxxoxxkOOOOOOOOO0kkOxddxx,
+           ..',;:lxxo:..     .:c;;;;;;;;;lolloooooooddoodd0kkxkOOkxkOkdllc'.....
+ .';:codxkkkxddolc,.                     'dooloooddooolllddooooooolc:;.         
+'OKkdc;,'...                              :xklllldd'':.  ;,                     
+,OKc.                                     :dklloodo;',;.';.                     
+.OKl                                      '0kdddddl.                            
+.OKd                                      ;0kdddxxl.                            
+ x0x.                                     ,0kxxxxxl.                            
+ o0O.                                      'ldddddc.                            
+ .c:.                                        'cccc,                             
+                                             'cccc,                             
+                                             .:ccc.                             
+                                              ....                              
+""")
+print('''
+                              mmmmmm    mmm   mm  mmmmmmmm     mmmm  
+                              ##""""##  ###   ##  """##"""   ##""""# 
+                              ##    ##  ##"#  ##     ##     ##       
+                              #######   ## ## ##     ##     ##  mmmm 
+                              ##    ##  ##  #m##     ##     ##  ""## 
+                              ##mmmm##  ##   ###     ##      ##mmm## 
+''')
