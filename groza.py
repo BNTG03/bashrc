029a9817cd0dcd6f9c4f89258e1d80ec4a50a5b9
BNTG03 <48709727+BNTG03@users.noreply.github.com>
2019-08-05 15:08:33 +0000

    Add files via upload

---
+++ groza.py
@@ -0,0 +1,28 @@
+print ("""
+                    .'       .....',;;;;::'                                    
+                    .;:;'''''''''','','cod:.                                    
+                    .,:.                dd.;.                                   
+                    .lo:;:::,,'...'''':lcl:ldlll:;;;;;;;;c::;;;;;lcccc:oxO00c:  
+                    .odkxoooolllooooddxOddxoddddddxxxdddxxxxxxkkxxxxxxxxxkKXxo  
+  .:;;;;;;;;;;;;;;::xxdkkdooollooooolododdddddddddddddddddxxxddxxxkkkkkkOkOKxl  
+  ;kk:,,,;xkOc,,,,;;cdo;;;:lccccccc:l::cllllllllloddddooooooooooooddddddddx0xc  
+  .l'     odK.       .,    :.cxkO0K0;           ;ool:lxk:kkc       .....l00Kxl  
+         .dd0:             . .,oxkkO.           ;lc;;cdO'oo;             oOKkl  
+         ;xx0l                 'xddkx           cllclokk .                .c0o  
+         ,kk0l                  :oddkl         .lllldxOd                        
+         .kx0;                   clddx;        'cllodxO;                        
+          '.,                    .ldxkx        cllodxkk                         
+                                  .'.         ;lloddkOc                         
+                                              :codxxkk                          
+                                                  ..,.
+""")
+print ('''
+                    mmmmmm    mmm   mm  mmmmmmmm     mmmm  
+                    ##""""##  ###   ##  """##"""   ##""""# 
+                    ##    ##  ##"#  ##     ##     ##        
+                    #######   ## ## ##     ##     ##  mmmm 
+                    ##    ##  ##  #m##     ##     ##  ""## 
+                    ##mmmm##  ##   ###     ##      ##mmm## 
+                    """""""   ""   """     ""        """"  
+''')
+
